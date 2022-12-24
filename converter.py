de2775ab339227a59a01eda6bcfa79e8ec0db28b
32-git <110297700+32-git@users.noreply.github.com>
2022-12-24 13:21:31 +0100

    added swap and home buttons

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 import units
 
 root = ctk.CTk()
@@ -8,7 +9,7 @@ root.geometry("550x400")
 root.configure(bg="#19191a")
 ctk.set_appearance_mode("dark")
 # ----------------------------------------
-font = ("OCR A Extended", -13.5) # font for all dropdowns
+font = ("OCR A Extended", -13.35) # font for all dropdowns
 
 def clear_frame():
     for widget in root.winfo_children():
@@ -40,64 +41,79 @@ def menu():
             root.geometry("650x450")
 
             # Make the title
-            title = ctk.CTkLabel(root, text=choice, text_font=("Courier", 33, 'bold'), padx=10, pady=7.5, 
-                                borderwidth=1.5, text_color="chartreuse3")
+            title = ctk.CTkLabel(root, text=choice, font=("Courier", 45, 'bold'), padx=10, pady=7.5, 
+                                text_color="chartreuse3")
             title.place(relx=0.5, rely=0.15, anchor='center')
+
+            def home():
+                clear_frame()
+                menu()
+            
+            home_img = ctk.CTkImage(dark_image=Image.open("menu.png"), size=(50,50))
+            home_button = ctk.CTkButton(master=root, image=home_img, text=None, fg_color='transparent',
+                                        width=50, height=50, hover_color='grey25', command=home)
+            home_button.place(relx=0.075, rely = 0.1, anchor='center')
 
             # Dropdowns - left and right for unit selections
             values = values()
 
             units_left = ctk.CTkComboBox(master=root, values=values,
-                                        width=220, height=30, corner_radius=10, text_font=("OCR A Extended", -13.75),
-                                        text_color="chartreuse3", dropdown_text_font=font, dropdown_text_color="chartreuse3")
+                                        width=220, height=30, corner_radius=10, font=("OCR A Extended", -13.35),
+                                        text_color="chartreuse3", dropdown_font=font, dropdown_text_color="chartreuse3")
             units_left.place(relx=0.25, rely=0.375, anchor='center')
             
             units_right = ctk.CTkComboBox(master=root, values=values,
-                                        width=220, height=30, corner_radius=10, text_font=("OCR A Extended", -13.75),
-                                        text_color="chartreuse3", dropdown_text_font=font, dropdown_text_color="chartreuse3")
+                                        width=220, height=30, corner_radius=10, font=("OCR A Extended", -13.35),
+                                        text_color="chartreuse3", dropdown_font=font, dropdown_text_color="chartreuse3")
             units_right.place(relx=0.75, rely=0.375, anchor='center')
 
-            # Code for a (possible) swap button:
-                # units_left.set(units_right.get())
-                # units_right.set(units_left.get())
+            def swap():
+                right = units_right.get()
+                left = units_left.get()
+                units_left.set(right)
+                units_right.set(left)
+
+            swap_img = ctk.CTkImage(dark_image=Image.open("swap.png"), size=(50,50))
+            swap_button = ctk.CTkButton(master=root, image=swap_img, text=None, fg_color='transparent',
+                                        width=50, height=50, hover_color='grey25', command=swap)
+            swap_button.place(relx=0.5, rely = 0.5, anchor='center')
             
             units_left.set('--')
             units_right.set('--')          
 
             # User input
-            fields.input = ctk.CTkEntry(root, width=210, height=40, corner_radius=10, text_color="chartreuse3", 
-                                        text_font=("Courier New", 12))
+            fields.input = ctk.CTkEntry(root, width=210, height=40, corner_radius=10, 
+                                        text_color="chartreuse3", font=("Courier New", 15))
             fields.input.place(relx=0.25, rely=0.5, anchor='center')
-            frame = ctk.CTkFrame(root, width=210, height=40, corner_radius=10, fg_color="grey24", border_width=2, 
-                                border_color="grey32")
+            frame = ctk.CTkFrame(root, width=210, height=40, corner_radius=10, fg_color="grey24", 
+                                    border_width=2, border_color="grey32")
             frame.place(relx=0.75, rely=0.5, anchor='center')
 
             # Output display in form of a label
-            output = ctk.CTkLabel(frame, corner_radius=10, text='', text_color="chartreuse3", text_font=("Courier New", 12))
+            output = ctk.CTkLabel(frame, corner_radius=10, text='', text_color="chartreuse3", font=("Courier New", 15))
             output.place(relx=0.5, rely=0.5, anchor='center')
 
             def convert():
                 left = units.unit_mappings[choice][units_left.get()]
                 right = units.unit_mappings[choice][units_right.get()]
-
                 result = units.convert(choice, left, right, fields.input.get())
                 output.configure(text=result)
 
             # Calculate button
             calc = ctk.CTkButton(master=root, width=120, height=32, border_width=2, corner_radius=8, text="Calculate",
-                                    text_font=("OCR A Extended", 13, 'bold'), text_color="chartreuse3", fg_color="grey27", 
-                                    border_color="chartreuse4", hover_color=None, command=convert)
+                                    font=("OCR A Extended", 13, 'bold'), text_color="chartreuse3", fg_color="grey27", 
+                                    border_color="chartreuse4", hover_color='grey25', command=convert)
             calc.place(relx=0.5, rely=0.7, anchor='center')
 
     # Make the title
-    title = ctk.CTkLabel(root, text="Converter", text_font=("Courier", 33, 'bold'), padx=10, pady=7.5, 
-                                borderwidth=1.5, text_color="chartreuse3")
+    title = ctk.CTkLabel(root, text="Converter", font=("Courier", 45, 'bold'), padx=10, pady=7.5, 
+                                text_color="chartreuse3")
     title.place(relx=0.5, rely=0.15, anchor='center')
 
     # Dropdown to choose field of units
     combobox = ctk.CTkComboBox(master=root, values=["--", "Length", "Area", "Volume", "Mass", "Temperature"],
                                     width=200, height=30, corner_radius=8, command=fields, text_color="chartreuse3",
-                                    dropdown_text_font=font, dropdown_text_color="chartreuse3")
+                                    dropdown_font=font, dropdown_text_color="chartreuse3")
     combobox.place(relx=0.5, rely=0.5, anchor='center')
     combobox.set("--") # default option: '--'
 
